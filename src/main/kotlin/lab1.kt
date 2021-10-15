f5216839bc14c1dd79f2b6045af5a13999db8696
CyberSet <sashapiter2015@gmail.com>
2021-10-15 18:12:44 +0300

    Finish left alignment

--- src/main/kotlin/lab1.kt
+++ src/main/kotlin/lab1.kt
@@ -11,16 +11,21 @@ fun alignText(
     alignment: Alignment = Alignment.LEFT
 ): String {
     var result = ""
-    for(curLine in text.lines()) {
-        var noSpaces = curLine.trim()
-        while(noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
-        while(noSpaces.length > lineWidth){
-            var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
-            if (lastSpace == -1) lastSpace = lineWidth - 1
-            result += noSpaces.substring(0 until lastSpace + 1) + '\n'
-            noSpaces = noSpaces.drop(lastSpace + 1)
+    when{
+        (alignment == Alignment.LEFT) -> {
+            for(curLine in text.lines()) {
+            var noSpaces = curLine.trim()
+            while(noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
+            while(noSpaces.length > lineWidth){
+                var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
+                if (lastSpace == -1) lastSpace = lineWidth - 1
+                result += noSpaces.substring(0 until lastSpace + 1) + '\n'
+                noSpaces = noSpaces.drop(lastSpace + 1)
+            }
+            result += noSpaces + '\n'
+            }
         }
-        result += noSpaces + '\n'
+        }
     }
     return  result
 }
