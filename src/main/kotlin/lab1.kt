0c4c62b16827b2bb596149cfb958dc8b643d56f2
CyberSet <sashapiter2015@gmail.com>
2021-10-24 00:42:32 +0300

    Remove last endline

--- src/main/kotlin/lab1.kt
+++ src/main/kotlin/lab1.kt
@@ -14,17 +14,17 @@ fun alignText(
     when{
         (alignment == Alignment.LEFT) -> {
             for(curLine in text.lines()) {
-            var noSpaces = curLine.trim()
-            while(noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
-            while(noSpaces.length > lineWidth){
-                var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
-                if (lastSpace == -1) lastSpace = lineWidth
-                result += noSpaces.substring(0 until lastSpace) + '\n'
-                noSpaces = noSpaces.drop(lastSpace + 1)
-            }
-            result += noSpaces + '\n'
+                var noSpaces = curLine.trim()
+                while(noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
+                while(noSpaces.length > lineWidth){
+                    var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
+                    if (lastSpace == -1) lastSpace = lineWidth
+                    result += noSpaces.substring(0 until lastSpace) + '\n'
+                    noSpaces = noSpaces.drop(lastSpace + 1)
+                }
+                result += noSpaces + '\n'
             }
         }
     }
-    return  result
+    return result.removeSuffix("\n")
 }
