4a455bd890115b189ad8188ea6f1c7d2167b5d6f
Klementina424 <KnaubKV@mail.ru>
2021-10-26 18:57:14 +0300

    Edit test

--- src/test/kotlin/Lab1Test.kt
+++ src/test/kotlin/Lab1Test.kt
@@ -1,56 +1,71 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+import java.lang.Exception
 
 internal class Lab1Test {
 
     @Test
     fun testSimpleLeftAlignment() {
-        val expected : String = """Умом — Россию не понять,
+        val expected: String = """Умом — Россию не понять,
 Аршином общим не измерить.
 У ней особенная стать —
 В Россию можно только верить."""
-        var result : String = alignText("""     Умом — Россию не понять,
+        val result: String = alignText(
+            """     Умом — Россию не понять,
 Аршином     общим не измерить.
     У   ней особенная стать —                                      
-В Россию     можно только верить.                   """, 40, Alignment.LEFT)
+В Россию     можно только верить.                   """, 40
+        )
         assertEquals(expected, result)
     }
 
     @Test
     fun testBlankLeftAlignment() {
-        val expected : String = ""
-        var result : String = alignText("     ")
+        val expected: String = ""
+        val result: String = alignText("     ")
         assertEquals(expected, result)
     }
 
     @Test
     fun testWordLongerThanLineLeftAlignment() {
-        val expected : String = """
+        val expected: String = """
             абра
             када
             бра
         """.trimIndent()
-        var result : String = alignText("абракадабра", 4, Alignment.LEFT)
+        val result: String = alignText("абракадабра", 4)
         assertEquals(expected, result)
     }
 
     @Test
     fun testWordEqualsLineLeftAlignment() {
-        val expected : String = """
+        val expected: String = """
             Равенство
         """.trimIndent()
-        var result : String = alignText("Равенство", 9, Alignment.LEFT)
+        val result: String = alignText("Равенство", 9)
         assertEquals(expected, result)
     }
 
     @Test
     fun testLineShiftLeftAlignment() {
-        val expected : String = """
+        val expected: String = """
             Шла Саша по шоссе и сосала
             сушку
         """.trimIndent()
-        var result : String = alignText("Шла Саша по шоссе и сосала сушку", 26, Alignment.LEFT)
+        val result: String = alignText("Шла Саша по шоссе и сосала сушку", 26)
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun testSmallLineWidthLeftAlignment() {
+        var result: Exception? = null
+        try {
+            alignText("Неважно", 0)
+        } catch (e: Exception) {
+            result = e
+        }
+        assertNotNull(result)
     }
 }
