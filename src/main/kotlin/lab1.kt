7fc57a94459f9202fab6030e203ba613fae854ff
CyberSet <sashapiter2015@gmail.com>
2021-10-26 18:49:51 +0300

    Remove unnecessary code

--- src/main/kotlin/lab1.kt
+++ src/main/kotlin/lab1.kt
@@ -1,34 +1,23 @@
-enum class Alignment {
-    LEFT,
-    RIGHT,
-    CENTER,
-    JUSTIFY
-}
-
 fun alignText(
     text: String,
     lineWidth: Int = 120,
-    alignment: Alignment = Alignment.LEFT
 ): String {
+    if(lineWidth < 1) throw IllegalArgumentException("Line width should be greater than 1")
     var result = ""
-    when{
-        (alignment == Alignment.LEFT) -> {
-            for(curLine in text.lines()) {
-                var noSpaces = curLine.trim()
-                while(noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
-                while(noSpaces.length > lineWidth){
-                    var spaceShift = 1
-                    var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
-                    if (lastSpace == -1) {
-                        lastSpace = lineWidth
-                        spaceShift = 0
-                    }
-                    result += noSpaces.substring(0 until lastSpace) + '\n'
-                    noSpaces = noSpaces.drop(lastSpace + spaceShift)
-                }
-                result += noSpaces + '\n'
+    for (curLine in text.lines()) {
+        var noSpaces = curLine.trim()
+        while (noSpaces.contains("  ")) noSpaces = noSpaces.replace("  ", " ")
+        while (noSpaces.length > lineWidth) {
+            var spaceShift = 1
+            var lastSpace = noSpaces.substring(0 until lineWidth + 1).lastIndexOf(' ')
+            if (lastSpace == -1) {
+                lastSpace = lineWidth
+                spaceShift = 0
             }
+            result += noSpaces.substring(0 until lastSpace) + '\n'
+            noSpaces = noSpaces.drop(lastSpace + spaceShift)
         }
+        result += noSpaces + '\n'
     }
     return result.removeSuffix("\n")
 }
