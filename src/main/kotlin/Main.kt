e25a7d18ecd42b63abffaea9fef6a07005188616
CyberSet <sashapiter2015@gmail.com>
2021-11-23 22:01:50 +0300

    Add calculating

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 fun main() {
-    var text : String = """Умом — Россию не понять,
-   Аршином   общим не измерить.
-У   ней особенная стать —                                      
-В Россию можно только верить. абракадрамымратакая
-    """.trimIndent();
-    text = alignText(text, 16, Alignment.LEFT);
-    println(text);
+    var result: String = parseToReversePolishNot("3+4*2/(1-5)^2")
+    println(result)
+    println(calculateReversePolishNot(result))
+    result = parseToReversePolishNot("-(1+2)*4+3")
+    println(result)
+    println(calculateReversePolishNot(result))
+    result = parseToReversePolishNot("10-5")
+    println(result)
+    println(calculateReversePolishNot(result))
+    result = parseToReversePolishNot("2+2.2+3+4-2+1-6*3.2")
+    println(result)
+    println(calculateReversePolishNot(result))
 }
--- src/main/kotlin/lab2.kt
+++ src/main/kotlin/lab2.kt
@@ -1,13 +1,19 @@
 import java.util.*
+import kotlin.math.pow
 
-enum class Operation(val textValue: String, val priorityValue: Int) {
-    OPENBRACKET("(", 0),
-    CLOSEBRACKET(")", 0),
-    PLUS("+", 1),
-    MINUS("-", 1),
-    MULTIPLY("*", 2),
-    DIVISION("/", 2),
-    POW("^", 3)
+enum class Operation(
+    val textValue: String,
+    val priorityValue: Int,
+    val isWaitedToUnary: Boolean,
+    val action: (Double, Double) -> Double
+) {
+    OPENBRACKET("(", 0, true, { a, b -> 0.0 }),
+    CLOSEBRACKET(")", 0, false, { a, b -> 0.0 }),
+    PLUS("+", 1, false, { a, b -> a + b }),
+    MINUS("-", 1, false, { a, b -> b - a }),
+    MULTIPLY("*", 2, true, { a, b -> a * b }),
+    DIVISION("/", 2, true, { a, b -> b / a }),
+    POW("^", 3, true, { a, b -> b.pow(a) })
 }
 
 fun getOperation(textValue: String): Operation {
@@ -29,32 +35,42 @@ fun parseToReversePolishNot(expression: String): String {
     var tempDigit: String = ""
     var tempOperation: String = ""
     var curOperation: Operation
+    var isAbleToUnary: Boolean = true
 
     for (curChar in expression) {
-        if (curChar.isDigit()) {
-            tempDigit = tempDigit.plus(curChar)
-            tempOperation = ""
-        } else {
-            tempOperation = tempOperation.plus(curChar)
-            curOperation = getOperation(tempOperation)
-            if(curOperation.priorityValue != 0) {
-                polishNot = polishNot.plus(tempDigit + ' ')
-                tempDigit = ""
-            }
-            if (curOperation == Operation.CLOSEBRACKET) {
-                while (operationsStack.isNotEmpty() && operationsStack.peek() != Operation.OPENBRACKET) {
-                    polishNot = polishNot.plus(operationsStack.peek().textValue + ' ')
-                    operationsStack.pop()
-                }
-                operationsStack.pop()
+        if (!curChar.isWhitespace()) {
+            if (curChar.isDigit() || curChar == '.') {
+                tempDigit = tempDigit.plus(curChar)
+                tempOperation = ""
+                isAbleToUnary = false
             } else {
-                while(operationsStack.isNotEmpty() && curOperation != Operation.OPENBRACKET && operationsStack.peek().priorityValue >= curOperation.priorityValue){
-                    polishNot = polishNot.plus(operationsStack.peek().textValue + ' ')
-                    operationsStack.pop()
+                tempOperation = tempOperation.plus(curChar)
+                curOperation = getOperation(tempOperation)
+                if (tempDigit.isNotBlank()) polishNot = polishNot.plus(tempDigit + ' ')
+                tempDigit = ""
+                if (isAbleToUnary && (curOperation == Operation.PLUS || curOperation == Operation.MINUS)) {
+                    if (curOperation == Operation.MINUS) polishNot = polishNot.plus("-1 ")
+                    else polishNot = polishNot.plus("1 ")
+                    operationsStack.push(Operation.MULTIPLY)
+                    isAbleToUnary = false
+                } else {
+                    if (curOperation == Operation.CLOSEBRACKET) {
+                        while (operationsStack.isNotEmpty() && operationsStack.peek() != Operation.OPENBRACKET) {
+                            polishNot = polishNot.plus(operationsStack.peek().textValue + ' ')
+                            operationsStack.pop()
+                        }
+                        operationsStack.pop()
+                    } else {
+                        while (operationsStack.isNotEmpty() && curOperation != Operation.OPENBRACKET && operationsStack.peek().priorityValue >= curOperation.priorityValue) {
+                            polishNot = polishNot.plus(operationsStack.peek().textValue + ' ')
+                            operationsStack.pop()
+                        }
+                        operationsStack.push((curOperation))
+                    }
+                    isAbleToUnary = curOperation.isWaitedToUnary
                 }
-                operationsStack.push((curOperation))
+                tempOperation = ""
             }
-            tempOperation = ""
         }
     }
     polishNot = polishNot.plus(tempDigit + ' ')
@@ -63,4 +79,29 @@ fun parseToReversePolishNot(expression: String): String {
         operationsStack.pop()
     }
     return polishNot
+}
+
+fun calculateReversePolishNot(expression: String): Double {
+    var digitStack: Stack<Double> = Stack()
+    var tempDigit: String = ""
+    var tempOperation: String = ""
+    var curOperation: Operation
+
+    for (curChar in expression) {
+        if (curChar.isWhitespace()) {
+            if (tempDigit.isNotBlank()) digitStack.push(tempDigit.toDouble())
+            tempDigit = ""
+        } else {
+            if (curChar.isDigit() || curChar == '.')
+                tempDigit = tempDigit.plus(curChar)
+            else {
+                tempOperation = tempOperation.plus(curChar)
+                curOperation = getOperation(tempOperation)
+                tempDigit = ""
+                digitStack.push(curOperation.action(digitStack.pop(), digitStack.pop()))
+            }
+            tempOperation = ""
+        }
+    }
+    return digitStack.peek()
 }
